a788c38ee7d835d6b65d183c6ccb1b1d5850edc3
Nate Nelson <7nater.nelson@gmail.com>
2021-06-01 10:46:20 -0600

    commit tree b76fce7fce593ab288ab5f57b6f01d20b76bd1de

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  renderMovie = (movie) => {
+    return <div>
+      <span>{movie.title}</span>
+      <span>{movie.time}</span>
+      <ul>{movie.genres.map(g => <li>g</li>)}</ul>
+    </div>
+  }
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(m => this.renderMovie(m))}
     </div>
   );
 };
